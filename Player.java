238c01f49b662bf8623dd825ee09a240db80d032
TheDankDork <150256710+TheDankDork@users.noreply.github.com>
2023-12-11 21:35:38 -0500

    Created basic outline
    Created basic outline for the player (will be a space shooter game).

---
+++ Player.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  
+
+public class Player extends Actor{
+    public int size;
+    public int speed;
+    public int bullets;
+    
+    public Player(int size, int speed){
+        this.size = size;
+        this.speed = speed;
+    }
+    
+    public void act(){
+        
+    }
+}
