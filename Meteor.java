680efa147367d1b7a09c2feb2cbf9944914a765b
TheDankDork <evan.xiyc@gmail.com>
2023-12-13 09:28:22 -0500

    Added basic meteor class
    Created a basic meteor class that allows a meteor to fall from the sky.

---
+++ Meteor.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  
+
+public class Meteor extends Actor{
+    public int xPos;
+    public int yPos;
+    public static int points = 1;
+    public static int livesTaken = 1;
+    
+    public Meteor(int xPos, int yPos, int points, int livesTaken){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.points = points;
+        this.livesTaken = livesTaken;
+    }
+    
+    public void act(){
+        setLocation(xPos, yPos); // Set location of the meteor
+        
+        MyWorld world = (MyWorld) getWorld();
+        yPos += (2 + world.speedMod); // Allows the meteor to slowly fall down
+        
+        // Remove self and take away lives if touching ground
+        if(isTouchingGround()){
+            world.modifyLives(-livesTaken);
+            world.removeObject(this);             
+        }
+    }
+    
+    private boolean isTouchingGround(){
+        if(yPos >= 790){ // Check the y position for the ground
+            return true;
+        }
+        return false;
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,12 @@
 import greenfoot.*;  
 
 public class MyWorld extends World{
+    public int score = 0;
+    public int lives = 3;
+    public int speedMod = 0;  // speedMod allows the game to gradually increase speed
+    
     Player player = new Player(1, 4, 1);
+    Meteor basic = new Meteor(0, 0, 1, 1);
     
     
     public MyWorld(){    
@@ -10,5 +15,15 @@ public class MyWorld extends World{
     
     public void act(){
         addObject(player, 200, 700);
+        addObject(basic, 0, 0);
+    }
+    
+    public void modifyScore(int points){    
+        score += points;
+    }
+    
+    // Modify lives based on meteor that was leaked
+    public void modifyLives(int amount){
+        lives += amount;    
     }
 }
