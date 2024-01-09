e02acc59f41700e3daddfed0e447f13769bed74c
TheDankDork <evan.xiyc@gmail.com>
2024-01-09 09:27:28 -0500

    Added explosion
    Added explosion and moved meteors in separate subclasses

--- Meteor.java
+++ Meteor.java
@@ -36,17 +36,36 @@ public class Meteor extends SmoothMover{
             if(isTouching(Bullet.class)){
                 health--;
                 if(health == 0){
+                    explode(50, 50);
                     world.score += points;
                 }
             }            
         }
-
     }
     
-    private boolean isTouchingGround(){
+    public boolean isTouchingGround(){
         if(yPos >= 790){ // Check the y position for the ground
             return true;
         }
         return false;
+    }
+    
+    public void explode(int scaleX, int scaleY){ 
+        int time = 0;
+        int frame = 0;
+        
+        GreenfootImage[] Images = new GreenfootImage[25];
+        for(int i = 0; i < 25; i++) {
+            Images[i] = new GreenfootImage("images/explosions/explosion" + i +".png");
+        }
+        
+        while(frame < 24){
+            time++;
+            
+            if((time % 10) == 0){
+                frame++;
+            }
+            setImage(Images[frame]);
+        }
     }
 }
