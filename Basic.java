8081ff3ec0431d471c993e124b23a5b100a3203f
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-01-10 08:50:02 -0500

    Fix explosion animation

--- Basic.java
+++ Basic.java
@@ -6,29 +6,29 @@ public class Basic extends Meteor
         super(image, scale, rotation, xPos, yPos, health, speed, points, livesTaken);
     }
     
-    public void act(){
-        SpaceShooter world = (SpaceShooter) getWorld();
+    // public void act(){
+        // SpaceShooter world = (SpaceShooter) getWorld();
         
-        if(health == 0){
-            explode(50, 50);
-            world.removeObject(this);
-        }
-        else{
-            yPos += (speed); // Allows the meteor to slowly fall down
-            setLocation(xPos, yPos); // Set location of the meteor
+        // if(health == 0){
+            // explode(50, 50);
+            // world.removeObject(this);
+        // }
+        // else{
+            // yPos += (speed); // Allows the meteor to slowly fall down
+            // setLocation(xPos, yPos); // Set location of the meteor
  
-            // Remove self and take away lives if touching ground
-            if(isTouchingGround() || isTouching(Player.class)){
-                world.modifyLives(-livesTaken);
-                health = 0;
-            }
-            if(isTouching(Bullet.class)){
-                health--;
-                if(health == 0){
-                    world.score += points;
-                }
-            }            
-        }
+            // // Remove self and take away lives if touching ground
+            // if(isTouchingGround() || isTouching(Player.class)){
+                // world.modifyLives(-livesTaken);
+                // health = 0;
+            // }
+            // if(isTouching(Bullet.class)){
+                // health--;
+                // if(health == 0){
+                    // world.score += points;
+                // }
+            // }            
+        // }
 
-    }
+    // }
 }
--- Meteor.java
+++ Meteor.java
@@ -7,8 +7,11 @@ public class Meteor extends SmoothMover{
     public double speed = 1;
     public int points = 1;
     public int livesTaken = 1;
-    GreenfootImage[] images = new GreenfootImage[25];   
-    
+    public boolean isExploding = false;
+
+    GreenfootImage[] images = new GreenfootImage[25];
+    SimpleTimer explosionTimer = new SimpleTimer();
+
     public Meteor(String image, int scale, int rotation, int xPos, int yPos, int health, double speed, int points, int livesTaken){
         setImage(image);
         this.xPos = xPos;
@@ -21,51 +24,51 @@ public class Meteor extends SmoothMover{
             images[i] = new GreenfootImage("images/explosions/explosion" + i +".png");
         } 
     }
-    
+
     public void act(){
         SpaceShooter world = (SpaceShooter) getWorld();
-        
-        if(health == 0){
-            world.removeObject(this);
+
+        yPos += (speed); // Allows the meteor to slowly fall down
+        setLocation(xPos, yPos); // Set location of the meteor
+
+        // Remove self and take away lives if touching ground
+        if(isTouchingGround() || isTouching(Player.class)){
+            world.modifyLives(-livesTaken);
+            health = 0;
         }
-        else{
-            yPos += (speed); // Allows the meteor to slowly fall down
-            setLocation(xPos, yPos); // Set location of the meteor
- 
-            // Remove self and take away lives if touching ground
-            if(isTouchingGround() || isTouching(Player.class)){
-                world.modifyLives(-livesTaken);
-                health = 0;
+        if(isTouching(Bullet.class)){
+            health--;
+            if(health == 0){
+                explosionTimer.mark();
+                isExploding = true;
+                world.score += points;
             }
-            if(isTouching(Bullet.class)){
-                health--;
-                if(health == 0){
-                    explode(50, 50);
-                    world.score += points;
-                }
-            }            
+        }            
+
+        if(isExploding) {
+            explode(50, 50);
         }
     }
-    
+
     public boolean isTouchingGround(){
         if(yPos >= 790){ // Check the y position for the ground
             return true;
         }
         return false;
     }
-    
+
+    int frame = 0;
     public void explode(int scaleX, int scaleY){ 
-        int frame = 0;
-        
-        SimpleTimer timer = new SimpleTimer();
-        timer.mark();
-        
-        while(frame < 24){           
-            if(timer.millisElapsed() > 40){
-                frame++;
-                timer.mark();
+
+        if(explosionTimer.millisElapsed() > 40){
+            frame += 1;
+            explosionTimer.mark();
+            if(frame == images.length){
+                getWorld().removeObject(this);
+            } else {
                 setImage(images[frame]);
             }
         }
+
     }
 }
